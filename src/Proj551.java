5f1f163078543c3789eeb950271a0123664ac2d2
JordanPlymale <jplymale@purdue.edu>
2020-09-17 22:29:11 -0400

    Cleaned up code, progress update

--- src/Proj551.java
+++ src/Proj551.java
@@ -12,6 +12,9 @@
 //378000000000
 //sum: 21685582737923
 
+//685000000000
+//sum: 40154741856665
+
 //10^8, 4047602471, 4043 ms
 
 public class Proj551 {
@@ -24,9 +27,9 @@ public class Proj551 {
 
     public static long a(long n) {
 
-        long sum = 21012650953103l;
+        long sum = 21685582737923L;
 
-        for(long i = 365600000000l; i < n; i++) {
+        for(long i = 378000000000L; i < n; i++) {
             if (i % 1000000000 == 0) {
                 System.out.println(i);
                 System.out.println("sum: " + sum);
@@ -40,12 +43,7 @@ public class Proj551 {
     public static long digitAdder(long num) {
 
         long finalNum = 0;
-        /*char[] temp = (num + "").toCharArray();
 
-        for (char c : temp) {
-
-            finalNum += (c - 48);
-        }*/
         while(num != 0){
             finalNum += num % 10;
             num /= 10;
@@ -56,38 +54,3 @@ public class Proj551 {
 
 
 }
-
-/*
-public class Proj551 {
-    private static final BigInteger HUNDRED_THOUSAND = new BigInteger("1000000");
-    public static void main(String[] args) {
-        System.out.println(a(new BigInteger((long)Math.pow(10,15)+"")));
-    }
-
-    public static BigInteger a(BigInteger n) {
-
-        BigInteger sum = BigInteger.ONE;
-
-        for(BigInteger i = BigInteger.ONE; i.compareTo(n) < 0; i = i.add(BigInteger.ONE)) {
-            if (i.mod(HUNDRED_THOUSAND).equals(BigInteger.ZERO)) {
-                System.out.println(i);
-                System.out.println("sum: " + sum);
-            }
-            sum = sum.add(digitAdder(sum));
-        }
-        return sum;
-    }
-
-
-    public static BigInteger digitAdder(BigInteger num) {
-        char[] temp = num.toString().toCharArray();
-        BigInteger finalNum = BigInteger.ZERO;
-
-        for (char c : temp) {
-
-            finalNum = finalNum.add(new BigInteger((c - 48) + ""));
-        }
-        return finalNum;
-    }
- */
-
